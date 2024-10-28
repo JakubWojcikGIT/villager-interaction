00790e391bd6adc5d6205466f588afaa1f60aab3
PawelHarasiuk <pharasiuk01@gmail.com>
2024-10-28 16:42:09 +0100

    clean up code

--- src/main/java/net/pawel/villagermod/entity/ModEntities.java
+++ src/main/java/net/pawel/villagermod/entity/ModEntities.java
@@ -12,11 +12,11 @@ import net.pawel.villagermod.entity.custom.ExtravertedVillagerEntity;
 import net.pawel.villagermod.entity.custom.IntrovertedVillagerEntity;
 
 public class ModEntities {
-    public static final EntityType<ExtravertedVillagerEntity> DIAMOND_VILLAGER = Registry.register(Registries.ENTITY_TYPE,
+    public static final EntityType<ExtravertedVillagerEntity> EXTRAVERTED_VILLAGER = Registry.register(Registries.ENTITY_TYPE,
             new Identifier(VillagerMod.MOD_ID, "extraverted_villager"),
             FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, ExtravertedVillagerEntity::new)
             .dimensions(EntityDimensions.fixed(1f, 2f)).build());
-    public static final EntityType<IntrovertedVillagerEntity> WOODEN_VILLAGER = Registry.register(Registries.ENTITY_TYPE,
+    public static final EntityType<IntrovertedVillagerEntity> INTROVERTED_VILLAGER = Registry.register(Registries.ENTITY_TYPE,
             new Identifier(VillagerMod.MOD_ID, "introverted_villager"),
             FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, IntrovertedVillagerEntity::new)
             .dimensions(EntityDimensions.fixed(1f, 2f)).build());
