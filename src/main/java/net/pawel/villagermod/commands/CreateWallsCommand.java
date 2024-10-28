82703bdd1ca4a89f2093fd32cc05d116899151ec
PawelHarasiuk <pharasiuk01@gmail.com>
2024-10-28 16:06:30 +0100

    cleaned up code

--- src/main/java/net/pawel/villagermod/commands/CreateWallsCommand.java
+++ src/main/java/net/pawel/villagermod/commands/CreateWallsCommand.java
@@ -6,7 +6,6 @@ import net.minecraft.block.Blocks;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
