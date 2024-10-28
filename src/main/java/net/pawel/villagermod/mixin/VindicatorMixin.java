827546d5285e613e1c96afb3a9b363cb8a3f73eb
PawelHarasiuk <pharasiuk01@gmail.com>
2024-10-28 14:50:03 +0100

    used mixin on Vindicator to make him attact our villagers

---
+++ src/main/java/net/pawel/villagermod/mixin/VindicatorMixin.java
@@ -0,0 +1,24 @@
+package net.pawel.villagermod.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.ActiveTargetGoal;
+import net.minecraft.entity.mob.IllagerEntity;
+import net.minecraft.entity.mob.VindicatorEntity;
+import net.minecraft.world.World;
+import net.pawel.villagermod.entity.custom.VillagerAbstract;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(VindicatorEntity.class)
+public abstract class VindicatorMixin extends IllagerEntity {
+    public VindicatorMixin(EntityType<? extends VindicatorEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "initGoals", at = @At("TAIL"))
+    private void initGoals(CallbackInfo ci) {
+        this.targetSelector.add(3, new ActiveTargetGoal<>(this, VillagerAbstract.class, true));
+    }
+}
